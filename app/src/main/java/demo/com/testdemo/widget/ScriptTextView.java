14fdc87eb17c88447d26fcb43fe2cfb5ced155a1
ludexiang <bubblelu@163.com>
2018-02-26 11:21:17 +0800

    script textview

--- app/src/main/java/demo/com/testdemo/widget/ScriptTextView.java
+++ app/src/main/java/demo/com/testdemo/widget/ScriptTextView.java
@@ -5,8 +5,8 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.FontMetrics;
-import android.graphics.Path;
-import android.graphics.Path.Direction;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.TextPaint;
@@ -22,6 +22,11 @@ public class ScriptTextView extends AppCompatTextView {
 
   private static final int DIRECTION_RIGHT = 0;
   private static final int DIRECTION_LEFT = 1;
+
+  private static final int LOC_TOP = 0;
+  private static final int LOC_MID = 1;
+  private static final int LOC_BOTTOM = 2;
+  private RectF rectF = new RectF();
   /**
    * 角标位置
    * default bottom
@@ -33,6 +38,7 @@ public class ScriptTextView extends AppCompatTextView {
    */
   private int scriptDirection;
   private String scriptText;
+  private int scriptSize;
 
   public ScriptTextView(Context context) {
     this(context, null);
@@ -48,29 +54,61 @@ public class ScriptTextView extends AppCompatTextView {
     scriptText = a.getString(R.styleable.ScriptTextView_script_text);
     scriptColor = a.getColor(R.styleable.ScriptTextView_script_color, getCurrentTextColor());
     scriptLoc = a.getInt(R.styleable.ScriptTextView_script_loc, 2);
+    scriptSize = a.getDimensionPixelSize(R.styleable.ScriptTextView_script_text_size, 5);
     scriptDirection = a.getInt(R.styleable.ScriptTextView_script_direction, DIRECTION_RIGHT);
     a.recycle();
   }
 
   @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    paint.setColor(scriptColor);
-    paint.setTextSize(20);
-    float textWidth = getTextWidth();
-
-    FontMetrics fontMetrics = paint.getFontMetrics();
-    float y = fontMetrics.top;
-    Path path = new Path();
-    if (scriptDirection == DIRECTION_RIGHT) {
-      path.addRect(textWidth, 0, textWidth+100, 100, Direction.CW);
-      canvas.drawTextOnPath(scriptText, path,0,0, paint);
-    }
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    RectF rect = getTextWidth((String) getText());
+    setMeasuredDimension((int) (rect.width() + scriptSize * 2), (int) rect.height());
+    rectF.set(rect);
   }
 
-  private float getTextWidth() {
+  @Override
+  protected void onDraw(Canvas canvas) {
+    int restore = canvas.save();
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    paint.setColor(scriptColor);
+    paint.setTextSize(scriptSize);
+    paint.setStyle(Style.FILL);
+
+    float x = 0f;
+    float y = 0f;
+    FontMetrics metrics = paint.getFontMetrics();
+    float scriptHeight = metrics.descent - metrics.ascent;
+    if (scriptDirection == DIRECTION_RIGHT) {
+      x = rectF.width() / 2 + 15;
+    } else if (scriptDirection == DIRECTION_LEFT) {
+      x = 0;
+    }
+    if (scriptLoc == LOC_TOP) {
+      y = (rectF.height() - scriptHeight) / 2 - Math.abs(metrics.ascent) / 6;
+    } else if (scriptLoc == LOC_MID) {
+      y = (rectF.height() - scriptHeight) / 2 + metrics.descent / 2;
+    } else if (scriptLoc == LOC_BOTTOM) {
+      y = (rectF.height() - scriptHeight) / 2 + metrics.descent; // ascent 是baseline 到字符最高度的距离因为在baseline之上则为负值
+    }
+
+    canvas.translate(x, y);
+    canvas.drawText(scriptText, x, y, paint);
+    canvas.restoreToCount(restore);
+
+    super.onDraw(canvas);
+  }
+
+  /**
+   * 测量文案 宽度及高度 返回Rect
+   */
+  private RectF getTextWidth(String measureText) {
+    RectF rectF = new RectF();
     TextPaint paint = getPaint();
-    return paint.measureText(getText(), 0, getText().length());
+    float measureWidth = paint.measureText(measureText);
+    FontMetrics metrics = paint.getFontMetrics();
+    float measureHeight = metrics.descent - metrics.ascent;
+    rectF.set(0, 0, measureWidth, measureHeight);
+    return rectF;
   }
 }
